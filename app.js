32d913ff89c7a5a1ca0f34776323f8e2ca8775ce
V1C0D3R <V1C0D3R@gmail.com>
2016-04-26 16:19:16 +0200

    Fix 'error' error

--- app.js
+++ app.js
@@ -10,10 +10,8 @@ var routes = require('./routes/routes.js');
 // *** Config Express *** //
 
 var app = express();
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
-
-app.use('/views',express.static(__dirname + '/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 
 // *** main routes *** //
@@ -24,38 +22,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-
-// *** error handlers *** //
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
 // *** Config Postgres *** //
--- routes/routes.js
+++ routes/routes.js
@@ -12,6 +12,14 @@ router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'../views/index.html'));
 });
 
+// catch 404 and forward to error handler
+router.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
 module.exports = router;
 
 // ** EXEMPLES BELOW **
