0e297d0cdf988a960bda8526d8d7028d4299e667
ccanete <cyril.canete@insa-lyon.fr>
2016-04-27 11:52:02 +0200

    refactor get user handling errors

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,23 +13,36 @@ var getAllUsers = function (req, res) {
 };
 module.exports.getAllUsers = getAllUsers;
 
+/**
+ * Fullfill with activities, booking and reminders a given user profile
+ * @param {string} username : the user username - required
+ * @param {string} email : the user email - required
+ * @param {string} password : the user password - required
+ * @param {string} citizen : the city where the user lives
+ * @param {string} age : the user age
+ * @return {Object} userId if creating, error else
+ */
 var addUser = function (req, res) {
   if (!req.query.username)
-    res.send('ERR: Missing params "username"');
+    res.status(500).send('ERROR: Missing params "username"');
   if (!req.query.email)
-    res.send('ERR: Missing params "email"');
+    res.status(500).send('ERROR: Missing params "email"');
   if (!req.query.password)
-    res.send('ERR: Missing params "password"');
-  return models.User.create({
+    res.status(500).send('ERROR: Missing params "password"');
+  models.User.create({
     username: req.query.username,
     email: req.query.email,
     password: req.query.password,
     citizen: req.query.citizen,
     age: req.query.age,
     tags: null,
-  }).then(function(user)  {
-    var user_id = user.get('id');
-    res.send({userId: user_id});
+  })
+  .then(function(user) {
+    return res.status(200).send({userId: user.get('id')});
+  })
+  .catch(function(err) {
+    console.error(err.stack);
+    return res.status(500).send('An error occured. User may already exists.');
   });
 };
 module.exports.addUser = addUser;
@@ -48,15 +61,12 @@ module.exports.deleteUser = deleteUser;
 var getUser = function(req, res) {
   //récuper les infos d'un user à partir de son id
   var userId = req.params.id;
-  models.User.findOne({
-    where: {
-      id: userId,
-    }
-  }).then(function (user) {
+  models.User.findOne({where: {id: userId,}})
+  .then(function (user) {
     if(user)
       res.send(user.toJSON());
     else
-      res.send('No User with the id :' + userId);
+      res.send('No User with this id :' + userId);
   });
 }
 module.exports.getUser = getUser;
