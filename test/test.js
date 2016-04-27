8215cc350995cff0667e9b2d978e6e6c1544c857
V1C0D3R <V1C0D3R@gmail.com>
2016-04-27 09:48:48 +0200

    Add first test

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+var helloWorldController = require('../controllers/helloworld.controller');
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe('Hello World Controller', function() {
+  describe('#getHelloWorld()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
