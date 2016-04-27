38fce5a361d1cc093285076030d09df0b5a31541
V1C0D3R <V1C0D3R@gmail.com>
2016-04-27 18:33:11 +0200

    activity.test.js formed but need to be filled

---
+++ test/activity.test.js
@@ -0,0 +1,40 @@
+var api = require('../app.js'),
+request = require('supertest')(api);
+
+describe('Activity Route', function() {
+  describe('#getActivities()', function () {
+    it('should return right json string', function (done) {
+    	// request
+     //    	.get('/activities')
+     //        .expect(200, done);
+    });
+  });
+
+  describe('#addActivity()', function () {
+    it('should return right json string', function (done) {
+    	// Fill with what you should send and receive
+    	//url : /activity
+    });
+  });
+
+  describe('#updateActivity()', function () {
+    it('should return right json string', function (done) {
+    	// Fill with what you should send and receive
+    	//url : /activity/:id
+    });
+  });
+
+  describe('#deleteActivity()', function () {
+    it('should return right json string', function (done) {
+    	// Fill with what you should send and receive
+    	//url : /activity/:id
+    });
+  });
+
+  describe('#getActivity()', function () {
+    it('should return right json string', function (done) {
+    	// Fill with what you should send and receive
+    	//url : /activity/:id
+    });
+  });
+});
