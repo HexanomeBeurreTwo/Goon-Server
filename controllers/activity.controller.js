417a00f43da3911a8d9372607b292ee18b140673
lchampavere <loic.champavere@insa-lyon.fr>
2016-04-29 09:07:02 +0200

    controller activity methods

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -9,8 +9,8 @@ var getActivities = function (req, res) {
 module.exports.getActivities = getActivities;
 
 /**
- * Controller for POST /user/
- * Creates a user in the database
+ * Controller for POST /activity/
+ * Creates a activity in the database
  * @param {string} name : the user name - required
  * @param {string} address 
  * @param {decimal} latitude 
@@ -22,35 +22,63 @@ module.exports.getActivities = getActivities;
  */
 var addActivity = function (req, res) {
 	//add an Activity
-  if (!req.query.name)
-    res.status(500).send('ERROR: Missing params "name"');
+	if (!req.body.name)
+		res.status(500).send('ERROR: Missing params "name"');
   models.Activity.sync().then(function () {
     models.Activity.create({
-		name: req.query.name.toLowerCase(),
-		description: req.query.description.toLowerCase(),
-		tags: req.query.tags.toLowerCase(),
-		address: req.query.address,
-		latitude: req.query.latitude,
-		longitude: req.query.longitude,
-		temporary: req.query.temporary,
-		date_start: req.query.date_start,
-		date_end: req.query.date_end,
-		opening_hours: null,
+		name: req.body.name.toLowerCase(),
+		description: req.body.description.toLowerCase(),
+		tags: req.body.tags,
+		address: req.body.address,
+		latitude: req.body.latitude,
+		longitude: req.body.longitude,
+		temporary: req.body.temporary,
+		date_start: req.body.date_start,
+		date_end: req.body.date_end,
+		opening_hours: req.body.opening_hours,
+		ActivityTypeId: req.body.ActivityTypeId,
     })
     .then(function(activity) {
-      return res.status(200).send({userId: activity.get('id')});
+      return res.status(200).json({activityId: activity.get('id')});
     })
     .catch(function(err) {
       console.error(err.stack);
-      return res.status(500).send('An error occured. Activity may already exists.');
+      return res.status(500).json({error: 'An error occured. Activity may already exists.'});
     });
   });
-}
+};
 module.exports.addActivity = addActivity;
 
+/**
+ * Controller for update /activity/:id
+ * Return a activity by id
+ * @param {interger} id : the activity id
+ * @return {User} activity if existing, error else
+ */
 var updateActivity = function (req, res) {
 	// update an activity
-}
+	var activityId = req.params.id;
+	var selector = { where: { id: activityId } };
+	var values = new Object();
+  if (req.body.name)
+    values.name = req.body.name;
+
+  if (values) {
+    models.Activity.findById(activityId).then(function(activity) {
+      return activity.update(values, selector);
+    }).then(function(activity) {
+      if(activity)
+        res.status(200).json(activity);
+      else
+        res.status(500).json({ error: 'No Activity with this id :' + activityId});
+    }).catch(function(err) {
+      console.error(err.stack);
+      res.status(500).json({ error: 'Error updating activity: '+ err});
+    });
+  } else  {
+      res.status(500).json({ error: 'No update because there are no name'});
+  }
+};
 module.exports.updateActivity = updateActivity;
 
 /**
@@ -60,15 +88,19 @@ module.exports.updateActivity = updateActivity;
  */
 var deleteActivity = function (req, res) {
 	//delete an activity via its id
-	var activityId = req.params.id;
-  	models.Activity.destroy({where: {id: activityId}, truncate: true})
-  	.then(function() {
-      return res.send('User with id'+ activityId +' deleted' );
-    })
-    .catch (function() {
-      return res.send('No User with this id :' + activityId);
-    });
-}
+  var activityId = req.params.id;
+  models.Activity.findById(activityId).then(function(activity) {
+    return activity.destroy();
+  }).then(function(activity) {
+    if(activity)
+      res.status(200).json({ message: 'Activity with id'+ activityId +' deleted' });
+    else
+      res.status(500).json({ error: 'No Activity with this id :' + activityId});
+  }).catch(function(error) {
+    console.log("ops: " + error);
+    res.status(500).json({ error: 'Error deleting activity: '+ error});
+  });
+};
 module.exports.deleteActivity = deleteActivity;
 
 /**
